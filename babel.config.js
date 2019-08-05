805f84d5f38de50d6583687b1829a7dadacaee04
VuongTran <vuong.tran@nfq.asia>
2019-08-05 15:38:45 +0700

    Change some package

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,27 @@
 module.exports = {
   presets: [
+    "@babel/react",
     [
       '@babel/preset-env',
       {
         targets: {
-          node: 'current',
+          browsers: [">0.25%"]
         },
       },
     ],
     '@babel/preset-typescript',
   ],
+  "plugins": [
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        ["@babel/preset-env", {
+          "modules": "commonjs"
+        }]
+      ]
+    }
+  }
 };
